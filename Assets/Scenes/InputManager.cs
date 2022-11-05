b3141dfb42e49277965bab091dc17b7452c92664
Guwahavel <andrewangaroni@outlook.com>
2022-11-04 20:29:17 -0500

    Tilemap and tilemap parsing
    Red tiles are functional

--- Assets/Scenes/InputManager.cs
+++ Assets/Scenes/InputManager.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class InputManager : MonoBehaviour
 {   
     GameObject player;
+    Tilemap tilemap;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        tilemap = GameObject.Find("TileMap").GetComponent<Tilemap>();
     }
 
     // Update is called once per frame
@@ -23,6 +26,12 @@ public class InputManager : MonoBehaviour
             player.SendMessage("Move", new Vector3(0,-1,0));
         } else if (Input.GetKeyDown(KeyCode.D)) {
             player.SendMessage("Move", new Vector3(1,0,0));
+        }
+
+        Vector3Int pos = new Vector3Int((int)Mathf.Floor(player.transform.position.x),(int)Mathf.Floor(player.transform.position.y));
+        TileBase tile = tilemap.GetTile(pos);
+        if (tile) {
+            //if (tile.name == "pink") {Debug.Log("ispink");}
         }
     }
 }
--- Assets/Scenes/PlayerUpdate.cs
+++ Assets/Scenes/PlayerUpdate.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerUpdate : MonoBehaviour
 {       
     bool isMoving;
     Vector3 targetPos;
     float moveSpeed;
+    Tilemap tilemap;
 
     // Start is called before the first frame update
     void Start()
     {
         isMoving = false;
-        moveSpeed = 0.05f;
+        moveSpeed = 0.2f;
+        tilemap = GameObject.Find("TileMap").GetComponent<Tilemap>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (isMoving) {
             transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed);
@@ -28,8 +31,12 @@ public class PlayerUpdate : MonoBehaviour
 
     public void Move(Vector3 vec) {
         if (!isMoving) {
-            isMoving = true;
-            targetPos = transform.position + vec;
+            Vector3Int newpos = new Vector3Int((int)Mathf.Floor(transform.position.x + vec.x),(int)Mathf.Floor(transform.position.y + vec.y));
+            TileBase tile = tilemap.GetTile(newpos);
+            if (!(tile && tile.name == "red")) {
+                isMoving = true;
+                targetPos = transform.position + vec;
+            }
         }
     }
 }
